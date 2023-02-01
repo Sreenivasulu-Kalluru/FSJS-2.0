79b1c47763984dc56d2516fc507be9046362505f
Sreenivasulu Kalluru <84690382+Sreenivasulu-Kalluru@users.noreply.github.com>
2023-02-01 14:00:29 +0530

    Update countries.js

--- JavaScript/19 - Question/countries.js
+++ JavaScript/19 - Question/countries.js
@@ -1,3 +1,6 @@
 const countries = ['India', 'Germany', 'Dubai', 'United Kingdom', 'France'];
 
-export countries;
+//exporting variable countries, then we can import it in other files
+
+// In commonJS Environment we use module.exports instead of export
+module.exports = countries;
