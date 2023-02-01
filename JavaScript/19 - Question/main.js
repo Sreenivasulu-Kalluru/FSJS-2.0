3fe68c83729924e2f1019a07eca864918f34a094
Sreenivasulu Kalluru <84690382+Sreenivasulu-Kalluru@users.noreply.github.com>
2023-02-01 14:03:19 +0530

    Update main.js

--- JavaScript/19 - Question/main.js
+++ JavaScript/19 - Question/main.js
@@ -1,5 +1,8 @@
-import { countries } from './countries';
-import { webTechs } from './webTechs';
+// In Node.js, JavaScript engine uses the commonJS environment which uses the require() function to import module
 
+const countries = require('./countries.js');
+const webTechs = require('./web_techs.js');
+
+//Accessing variables from countries.js & webTech.js
 console.log(countries);
 console.log(webTechs);
