fefd2d653347e2067c028e9881286cafc8ff96e4
Vasu Kalluru <84690382+KALLURUSREENIVASULU@users.noreply.github.com>
2023-03-08 09:47:41 +0530

    Code Changes in react-router app

--- React/react-router-multipage-project/src/components/Navbar.js
+++ React/react-router-multipage-project/src/components/Navbar.js
@@ -5,20 +5,23 @@ const Navbar = () => {
   return (
     <nav className="nav-items bg-[#333]">
       <ul className="flex p-4 justify-around items-center ">
-        <Link to="/Home">
-          <li className="btn hover:text-[#000] hover:bg-transparent hover:text-[#fff] hover:border-[2px] hover:border-[#fff]">
-            Home
-          </li>
+        <Link
+          to="/Home"
+          className="btn hover:text-[#000] hover:bg-transparent hover:text-[#fff] hover:border-[2px] hover:border-[#fff] focus:text-[#fff] focus:border-[2px] focus:border-[#fff]"
+        >
+          <li>Home</li>
         </Link>
-        <Link to="/About">
-          <li className="btn hover:text-[#000] hover:bg-transparent hover:text-[#fff] hover:border-[2px] hover:border-[#fff]">
-            About
-          </li>
+        <Link
+          to="/About"
+          className="btn hover:text-[#000] hover:bg-transparent hover:text-[#fff] hover:border-[2px] hover:border-[#fff] focus:text-[#fff] focus:border-[2px] focus:border-[#fff]"
+        >
+          <li>About</li>
         </Link>
-        <Link to="/Contact">
-          <li className="btn hover:text-[#000] hover:bg-transparent hover:text-[#fff] hover:border-[2px] hover:border-[#fff]">
-            Contact
-          </li>
+        <Link
+          to="/Contact"
+          className="btn hover:text-[#000] hover:bg-transparent hover:text-[#fff] hover:border-[2px] hover:border-[#fff] focus:text-[#fff] focus:border-[2px] focus:border-[#fff]"
+        >
+          <li>Contact</li>
         </Link>
       </ul>
     </nav>
